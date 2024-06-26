8d46275d236457f069b9d653e4f516ebb371c805
Andrew <you@example.com>
2024-06-26 09:27:24 -0400

    install script update

--- strato-install.sh
+++ strato-install.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+set -x  # Print each command before executing it
 
 # Ask user for mandatory variables
-
 read -p "Enter domain name: " DOMAIN_NAME
 read -p "Enter client ID: " CLIENT_ID
 read -p "Enter client secret: " CLIENT_SECRET
 
-sudo -i
+# Update and install packages
 apt update && apt upgrade -y
-sudo apt install certbot -y
+sudo apt install -y certbot docker.io git htop jq
 
-apt install -y docker.io git htop jq
+# Enable and start Docker
 systemctl enable docker
 systemctl start docker
 
+# Install docker-compose
 mkdir -p /usr/local/lib/docker/cli-plugins/
 apt install docker-compose
-docker-compose
 
 # Create the data directory
 sudo mkdir -p /datadrive
-
-# Set appropriate permissions
 sudo chown ${USER}:${USER} /datadrive
 sudo chmod 755 /datadrive
 
 # Set up Docker to use the new data directory
 sudo mkdir -p /datadrive/docker
 sudo mkdir -p /etc/docker
-sudo bash -c "echo '{\"data-root\": \"/datadrive/docker\"}' > /etc/docker/daemon.json"
+echo '{
+  "data-root": "/datadrive/docker"
+}' | sudo tee /etc/docker/daemon.json
 
 # If Docker is already installed, move existing data
 if [ -d "/var/lib/docker" ]; then
@@ -48,6 +49,7 @@ fi
 # Check available space
 df -h /datadrive
 
+# Clone and set up STRATO
 cd /datadrive || exit
 git clone https://github.com/blockapps/strato-getting-started
 cd strato-getting-started || exit
@@ -58,9 +60,9 @@ sudo ./strato --compose
 # Pull necessary Docker images
 sudo ./strato --pull
 
+# Create the run script
 cat <<EOF >strato-run.sh
 #!/bin/bash
-
 NODE_HOST="$DOMAIN_NAME" \\
 BOOT_NODE_IP='["44.209.149.47","54.84.33.40","52.1.78.10","44.198.14.117"]' \\
 networkID="6909499098523985262" \\
@@ -74,3 +76,4 @@ BASE_CODE_COLLECTION=d979d67877db869f18283e93ea4bf2d256df92d2 \\
 EOF
 
 sudo chmod +x strato-run.sh
+echo "Installation complete. You can now run ./strato-run.sh to start STRATO."

3dae34d07565f575d4a78749b6012441ec944953
nikitamendelbaum <nikitamendelbaum@users.noreply.github.com>
2024-10-10 21:17:53 +0700

    idempotent scripts, logging

--- ssl-get-cert.sh
+++ ssl-get-cert.sh
@@ -6,26 +6,49 @@ if [ "$#" -ne 2 ]; then
     exit 101
 fi
 
+LOGFILE="~/strato-setup.log"
+log_message() {
+    local MESSAGE=$1
+    echo "$(date +'%Y-%m-%d %H:%M:%S') SSL-GET-CERT : $MESSAGE" | tee -a "$LOGFILE"
+}
+
 if [ ! -d "/datadrive/strato-getting-started" ]; then
-  echo "Expected to have strato-getting-started at /datadrive/strato-getting-started, but the directory does not exist."
+  log_message "Expected to have strato-getting-started at /datadrive/strato-getting-started, but the directory does not exist."
   exit 102
 fi
 
 DOMAIN=$1
 EMAIL=$2
 
+NGINX_CONTAINER_NAME=$(sudo docker ps --format '{{.Names}}' | grep -E 'strato-nginx-1|strato_nginx_1')
+
 # Ignoring the existing certs if they exists
 rm -rf /tmp/letsencrypt && mv /etc/letsencrypt /tmp/
+
+# if nginx container is running - stop it temporarily for certbot execution
+[[ -n "$NGINX_CONTAINER_NAME" ]] && sudo docker stop $NGINX_CONTAINER_NAME
+log_message "nginx was temporarily stopped"
+function start_nginx {
+  [[ -n "$NGINX_CONTAINER_NAME" ]] && sudo docker start $NGINX_CONTAINER_NAME
+  log_message "nginx was started back"
+}
+# start the nginx back even if the certbot failed
+trap start_nginx EXIT
 sudo certbot certonly --standalone --preferred-challenges http --agree-tos --non-interactive --email "${EMAIL}" -d "${DOMAIN}"
+log_message "Certbot executed successfully"
+start_nginx
+trap - EXIT
 
 sudo cp /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /datadrive/strato-getting-started/ssl/certs/server.pem
 sudo cp /etc/letsencrypt/live/${DOMAIN}/privkey.pem /datadrive/strato-getting-started/ssl/private/server.key
 
-if [ "$(docker ps -a --filter name=strato-nginx-1 --format '{{.Names}}')" = "strato-nginx-1" ]; then
-  sudo docker cp --follow-link /datadrive/strato-getting-started/ssl/certs/server.pem strato-nginx-1:/etc/ssl/certs/server.pem
-  sudo docker cp --follow-link /datadrive/strato-getting-started/ssl/private/server.key strato-nginx-1:/etc/ssl/private/server.key
-  sudo docker exec strato-nginx-1 openresty -s reload
-  echo "Cert updated for the running node."
+log_message "Cert and key were copied to strato-getting-started directory"
+
+if [ -n "$NGINX_CONTAINER_NAME" ]; then
+  sudo docker cp --follow-link /datadrive/strato-getting-started/ssl/certs/server.pem ${NGINX_CONTAINER_NAME}:/etc/ssl/certs/server.pem
+  sudo docker cp --follow-link /datadrive/strato-getting-started/ssl/private/server.key ${NGINX_CONTAINER_NAME}:/etc/ssl/private/server.key
+  sudo docker exec ${NGINX_CONTAINER_NAME} openresty -s reload
+  log_message "Cert and key were updated on the running node."
 else
-  echo "No strato-nginx-1 container is running. STRATO is not yet started. Skipping the cert update for the running node."
+  log_message "No strato-nginx-1 (or strato_nginx_1) container is running. STRATO is not yet started. Skipping the cert update for the running node."
 fi

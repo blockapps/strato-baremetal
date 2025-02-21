fa721efdb704bfc8e7ecf4c77801be0b1a754d3d
Bob Summerwill <bob@summerwill.net>
2025-02-20 16:47:32 -0800

    Added missing IPv6 ingress for HTTPS.

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,6 +110,7 @@ resource "aws_security_group" "instance_sg" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
